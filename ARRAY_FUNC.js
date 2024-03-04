c8ef681c2924951b80322aaf90d2aebfce5ebaa4
Unni krishnan <uk3090@gmail.com>
2024-03-05 00:02:05 +0530

    Update ARRAY_FUNC.js

--- ARRAY_FUNC.js
+++ ARRAY_FUNC.js
@@ -189,3 +189,23 @@ function quickSort(arr) {
 
 const arr = [3, 1, 4, 1, 5, 9, 2, 6, 5];
 console.log(quickSort(arr));
+
+
+/////////////////////////////////////////////////////
+/////////////////////BUBBLE SORT////////////////////
+////////////////////////////////////////////////////
+function bubbleSort(arr){
+ let n = arr.length;
+  for(var i=0;i < n;i++) {
+    for(var j=0;j < (n-i-1);j++) {
+      if(arr[j] > arr[j+1])  {
+       var temp=arr[j];
+       arr[j]=arr[j+1];
+       arr[j+1]=temp; 
+     }
+    }
+  }
+  return arr;
+}
+var arr=[2,3,1,4,7,6];
+const sortedArray = bubbleSort(arr);
